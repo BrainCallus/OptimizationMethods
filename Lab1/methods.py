245f69954b9392f473d1b5868e2c08451c7d909a
natalia-nochevkina <90453557+natalia-nochevkina@users.noreply.github.com>
2023-03-22 22:54:37 +0300

    Update methods.py

--- Lab1/methods.py
+++ Lab1/methods.py
@@ -10,26 +10,24 @@ def line_search(x, p, f, grad):
     new_x = x + alf * p
     new_nabl = grad(new_x)
     f_new_x = f(new_x)
-    func_calls = 2
     while f_new_x > fx + (c1 * alf * nabl.T @ p) \
             or math.fabs(new_nabl.T @ p) > c2 * math.fabs(nabl.T @ p) and f_new_x != fx:
         alf *= 0.5
         new_x = x + alf * p
         f_new_x = f(new_x)
         new_nabl = grad(new_x)
-        func_calls += 1
-    return func_calls, alf
+    return alf
 
 def wolfe(x, eps, f, grad):
     dim = len(x)
-    steps_x = [x]
-    steps_y = [f(x)]
+    steps_arg = [x]
+    steps_f = [f(x)]
+    i = 1
     H = np.eye(dim)
     nabl = grad(x)
-    func_calls = 2
     while np.linalg.norm(nabl) > eps:
         p = -H @ nabl
-        f_c, alf = line_search(x, p, f, grad)
+        alf = line_search(x, p, f, grad)
         new_nabl = grad(x + alf * p)
         y = new_nabl - nabl
         y = np.array([y], dtype='float64')
@@ -44,15 +42,15 @@ def wolfe(x, eps, f, grad):
         H = hess_inter + (r * (s @ s.T))
         nabl = new_nabl[:]
         x = x+alf*p
-        func_calls += f_c + 1
-        steps_x.append(x)
-        steps_y.append(f(x))
-    return func_calls, steps_x, steps_y
+        steps_arg.append(x)
+        steps_f.append(f(x))
+        i += 1
+    return i, steps_arg, steps_f
 
 
 def gen_learning_rate(lr_step):
     def lr_func(*args, **kwarg):
-        return 0, lr_step
+        return lr_step
 
     return lr_func
 
@@ -65,9 +63,7 @@ def golden(x, eps, func, grad):
     xx1 = x - l1 * grr
     xx2 = x - l2 * grr
     f1, f2 = func(xx1), func(xx2)
-    i = 4
     while (b - a) / 2 >= eps:
-        i += 1
         if f1 < f2:
             b, l2, f2 = l2, l1, f1
             l1 = a + k1 * (b - a)
@@ -78,7 +74,7 @@ def golden(x, eps, func, grad):
             l2 = a + k2 * (b - a)
             xx2 = x - l2 * grr
             f2 = func(xx2)
-    return i, (a + b) / 2
+    return (a + b) / 2
 
 
 def using_grad_vector(method, x, eps, func, grad):
@@ -89,12 +85,12 @@ def using_grad_vector(method, x, eps, func, grad):
     i = 1
     while math.fabs(func_value - func_value_prev) > eps:
         func_value_prev = func_value
-        ii, lr = method(x, eps, func, grad)
+        lr = method(x, eps, func, grad)
         x = x - lr * grad(x)
         steps_x.append(list(x))
         steps_y.append(func_value)
         func_value = func(x)
-        i += 2 + ii
+        i += 1
     return i, steps_x, steps_y
 
 
@@ -103,10 +99,10 @@ def method_mas(method, x, iteration_numb, func, grad, eps=None):
         eps = 10
     xs = []
     ys = []
-    func_calls = 0
+    iterations = []
     for i in range(1, iteration_numb):
-        f_c, step_x, step_y = using_grad_vector(method, x, math.pow(eps, -i), func, grad)
+        iter, step_x, step_y = using_grad_vector(method, x, math.pow(eps, -i), func, grad)
         xs.append(step_x)
         ys.append(step_y)
-        func_calls += f_c
-    return func_calls, xs, ys
+        iterations.append(iter)
+    return iterations, xs, ys
