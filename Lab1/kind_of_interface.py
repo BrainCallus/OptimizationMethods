9ccab48f40488ec8619d71bd8657566f511b0b18
natalia-nochevkina <90453557+natalia-nochevkina@users.noreply.github.com>
2023-03-22 22:55:00 +0300

    Update kind_of_interface.py

--- Lab1/kind_of_interface.py
+++ Lab1/kind_of_interface.py
@@ -17,16 +17,18 @@ def analyze_function(n, func, grad, given_vector=None, vector_using=None):
     alpha = 5
     start = [alpha] * n
     eps = 10 ** (-4)
-    lr = 0.001
+    lr = 0.01
 
     if vector_using[0]:
+        if given_vector is not None:
+            start = given_vector
         learning_rate = gen_learning_rate(lr)
         points1 = using_grad_vector(learning_rate, start, eps, func, grad)
     else:
         points1 = None
 
     if vector_using[2]:
-        points3 = wolfe(start, 10**-3 * 2, func, grad)
+        points3 = wolfe(start, 10 ** -3 * 2, func, grad)
     else:
         points3 = None
 
@@ -45,20 +47,17 @@ def single_analyse_result(n, func, grad, given_vector=None):
 
     print('---')
     print("Градиентный спуск")
-    print('Функ: ' + str(points1[0]))
-    print('Итер: ' + str(len(points1[1])))
+    print('Итер: ' + str(points1[0]))
     print('Знач: ' + str(points1[2][-1]))
 
     print('---')
     print("Золотое сечение")
-    print('Функ: ' + str(points2[0]))
-    print('Итер: ' + str(len(points2[1])))
+    print('Итер: ' + str(points2[0]))
     print('Знач: ' + str(points2[2][-1]))
 
     print('---')
     print("Вульфе")
-    print('Функ: ' + str(points3[0]))
-    print('Итер: ' + str(len(points3[1])))
+    print('Итер: ' + str(points3[0]))
     print('Знач: ' + str(points3[2][-1]))
 
     print('---------------')
@@ -93,9 +92,9 @@ def draw_function_graph(func, grad, given_matrix=None, given_vector=None):
 
     ax = plt.subplot()
     ax.contour(x, y, func([x, y]), levels=sorted(set([func(i) for i in points1[1]])))
-    ax.plot(xs1, ys1, 'o-', label = 'learning_rate: ' + str(len(points1[1])))
-    ax.plot(xs2, ys2, 'o-', label = 'golden       : ' + str(len(points2[1])))
-    ax.plot(xs3, ys3, 'o-', label = 'wolfe        : ' + str(len(points3[1])))
+    ax.plot(xs1, ys1, 'o-', label = 'learning_rate: ' + str(points1[0]))
+    ax.plot(xs2, ys2, 'o-', label = 'golden       : ' + str(points2[0]))
+    ax.plot(xs3, ys3, 'o-', label = 'wolfe        : ' + str(points3[0]))
     ax.legend(prop='monospace')
 
     plt.show()
@@ -105,7 +104,7 @@ def multiple_tests_to_excel():
     file = './tables/multiple_results.xlsx'
     sheet = 'Испытания'
 
-    ii = [1, 2, 5, 10, 50, 100, 150, 200, 250, 300, 350, 400, 450,
+    ii = [2, 5, 10, 50, 100, 150, 200, 250, 300, 350, 400, 450,
           500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
     jj = [2, 5, 10, 50, 100, 150, 200, 250, 300, 350, 400, 450,
          500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
@@ -113,7 +112,7 @@ def multiple_tests_to_excel():
 
     frame = 2
     pause = 2
-    methods = 1
+    methods = 3
     times = 25
 
     rows = frame + len(ii) + 100
@@ -125,13 +124,13 @@ def multiple_tests_to_excel():
     table[0][1] = 'n'
     table[1][0] = 'k'
 
-    # table[frame + pause + len(jj)][0] = 'golden'
-    # table[frame + pause + len(jj)][1] = 'n'
-    # table[frame + pause + len(jj) + 1][0] = 'k'
-    #
-    # table[2 * frame + 2 * pause + 2 * len(jj)][0] = 'wolfe'
-    # table[2 * frame + 2 * pause + 2 * len(jj)][1] = 'n'
-    # table[2 * frame + 2 * pause + 2 * len(jj) + 1][0] = 'k'
+    table[frame + pause + len(jj)][0] = 'golden'
+    table[frame + pause + len(jj)][1] = 'n'
+    table[frame + pause + len(jj) + 1][0] = 'k'
+
+    table[2 * frame + 2 * pause + 2 * len(jj)][0] = 'wolfe'
+    table[2 * frame + 2 * pause + 2 * len(jj)][1] = 'n'
+    table[2 * frame + 2 * pause + 2 * len(jj) + 1][0] = 'k'
 
     it = -1
     for j in jj:
@@ -158,18 +157,16 @@ def multiple_tests_to_excel():
             for k in range(times):
                 a, func, grad = generate_random_function_vector(dimensions, self_number)
                 points1, points2, points3 = \
-                    analyze_function(dimensions, func, grad, vector_using=[True, False, False])
-                average[0] += len(points1[1])
-                # average[1] += len(points2[1])
-                # average[2] += len(points3[1])
+                    analyze_function(dimensions, func, grad, vector_using=[True, True, True])
+                average[0] += points1[0]
+                average[1] += points2[0]
+                average[2] += points3[0]
             average[0] = average[0] / times
-            # average[1] = average[1] / times
-            # average[2] = average[2] / times
+            average[1] = average[1] / times
+            average[2] = average[2] / times
             for k in range(methods):
                 table[iter2 + frame * (k + 1) + pause * k + len(jj) * k][iter1 + frame] = average[k]
             it += 1
-            print(it)
-            print(average[0])
 
     df = pd.DataFrame(table)
     df.to_excel(file, sheet_name=sheet)
@@ -180,12 +177,13 @@ def functions_compare(func1, grad1, func2, grad2, method=1, func_name=None):
     vector_using = [False, False, False]
     vector_using[method - 1] = True
     method_name = method_names[method - 1]
+    given_vector = [-150, 150]
+    al = 10
 
-    p1 = analyze_function(2, func1, grad1, vector_using=vector_using)
-    p2 = analyze_function(2, func2, grad2, vector_using=vector_using)
+    p1 = analyze_function(2, func1, grad1, vector_using=vector_using, given_vector=given_vector)
+    p2 = analyze_function(2, func2, grad2, vector_using=vector_using, given_vector=given_vector)
     points1 = p1[method - 1]
     points2 = p2[method - 1]
-
 
     np.seterr(invalid='ignore')
     np.seterr(over='ignore')
@@ -201,16 +199,16 @@ def functions_compare(func1, grad1, func2, grad2, method=1, func_name=None):
     up = max(max(ys1), max(ys2))
     bottom = min(min(ys1), min(ys2))
 
-    x0 = np.linspace(left - 1, right + 1, 1000)
-    y0 = np.linspace(bottom - 1, up + 1, 1000)
+    x0 = np.linspace(left - al, right + al, 1000)
+    y0 = np.linspace(bottom - al, up + al, 1000)
 
     x, y = np.meshgrid(x0, y0)
     plt.title(func_name)
     ax = plt.subplot()
     ax.contour(x, y, func1([x, y]), levels=sorted(set([func1(i) for i in points1[1]])))
     ax.contour(x, y, func2([x, y]), levels=sorted(set([func2(i) for i in points2[1]])))
-    ax.plot(xs1, ys1, 'o-', label = method_name + ': ' + str(len(points1[1])))
-    ax.plot(xs2, ys2, 'o-', label = method_name + ': ' + str(len(points2[1])))
+    ax.plot(xs1, ys1, 'o-', label = method_name + ': ' + str(points1[0]))
+    ax.plot(xs2, ys2, 'o-', label = method_name + ': ' + str(points2[0]))
     ax.legend(prop='monospace')
 
     plt.show()
@@ -246,33 +244,25 @@ def analyse_eps_parameter(func, grad):
     start = [-15, 15]
     lr = gen_learning_rate(0.06)
 
-    func_calls, p1, p2 = method_mas(lr, start, 9, func, grad)
+    iterations, p1, p2 = method_mas(lr, start, 9, func, grad)
 
-    print('---------------')
-    print('---------------')
     print('---------------')
     print('learning_rate')
     for i in range(len(p1)):
-        # print('---')
-        # print('eps=1e-0' + str(i + 1))
-        # print('Функ: ' + str(func_calls[i]))
-        # print('Итер: ' + str(len(p1[i])))
-        # print('Рез : ' + str(p2[i][-1]))
-        print(str(func_calls[i]))
+        print('---')
+        print('eps=1e-0' + str(i + 1))
+        print('Итер: ' + str(iterations))
+        print('Рез : ' + str(p2[i][-1]))
 
-    func_calls, p1, p2 = method_mas(golden, start, 9, func, grad)
+    iterations, p1, p2 = method_mas(golden, start, 9, func, grad)
 
-    print('---------------')
-    print('---------------')
     print('---------------')
     print('golden')
     for i in range(len(p1)):
-        # print('---')
-        # print('eps=1e-0' + str(i + 1))
-        # print('Функ: ' + str(func_calls[i]))
-        # print('Итер: ' + str(len(p1[i])))
-        # print('Рез : ' + str(p2[i][-1]))
-        print(str(func_calls[i]))
+        print('---')
+        print('eps=1e-0' + str(i + 1))
+        print('Итер: ' + str(iterations))
+        print('Рез : ' + str(p2[i][-1]))
 
 
 def graph_show_or_save(func, file_name=None, dir_name='graphs', save=True, show=False):
@@ -289,5 +279,3 @@ def graph_show_or_save(func, file_name=None, dir_name='graphs', save=True, show=
         Image.open(file_name).save(file_name, 'JPEG')
     if show:
         plt.show()
-
-
