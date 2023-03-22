b3b25575d17f01d68aafd8f968c2938b255baa37
natalia-nochevkina <90453557+natalia-nochevkina@users.noreply.github.com>
2023-03-22 23:00:23 +0300

    Update methods_with_grads.py

--- Lab1/methods_with_grads.py
+++ Lab1/methods_with_grads.py
@@ -21,45 +21,48 @@ def line_search(x, p, f):
     fx = f(x)
     new_x = x + alf * p
     new_nabl = gradient_in_the_point(f, new_x)
-    while f(new_x) > fx+(c1 * alf * nabl.T@p) or new_nabl.T@p<=c2*nabl.T@p:
-        alf*=0.5
-        new_x = x+alf*p
+    f_new_x = f(new_x)
+    while f_new_x > fx + (c1 * alf * nabl.T @ p) \
+            or math.fabs(new_nabl.T @ p) > c2 * math.fabs(nabl.T @ p) and f_new_x != fx:
+        alf *= 0.5
+        new_x = x + alf * p
+        f_new_x = f(new_x)
         new_nabl = gradient_in_the_point(f, new_x)
     return alf
 
 def wolfe(x, eps, f):
     dim = len(x)
-    steps_x = [x]
-    steps_y = [f(x)]
+    steps_arg = [x]
+    steps_f = [f(x)]
+    i = 1
     H = np.eye(dim)
-    i = 0
     nabl = gradient_in_the_point(f, x)
     while np.linalg.norm(nabl) > eps:
-        i += 1
-        p = -H@nabl
+        p = -H @ nabl
         alf = line_search(x, p, f)
-        s = alf*p
-        new_nabl = gradient_in_the_point(f, x+alf*p)
+        new_nabl = gradient_in_the_point(f, x + alf * p)
         y = new_nabl - nabl
         y = np.array([y], dtype='float64')
-        s = np.array([s], dtype='float64')
         y = np.reshape(y,(dim,1))
+        s = alf*p
+        s = np.array([s], dtype='float64')
         s = np.reshape(s,(dim,1))
-        r = 1/(y.T@s)
-        li = (np.eye(2) - (r * (s @ y.T)))
-        ri = (np.eye(2) - (r * (y @ s.T)))
-        hess_inter = li@H@ri
+        r = 1 / (y.T @ s)
+        li = (np.eye(dim) - (r * (s @ y.T)))
+        ri = (np.eye(dim) - (r * (y @ s.T)))
+        hess_inter = li @ H @ ri
         H = hess_inter + (r * (s @ s.T))
         nabl = new_nabl[:]
         x = x+alf*p
-        steps_x.append(x)
-        steps_y.append(f(x))
-    return i, steps_x, steps_y
+        steps_arg.append(x)
+        steps_f.append(f(x))
+        i += 1
+    return i, steps_arg, steps_f
 
 
 def gen_learning_rate(lr_step):
     def lr_func(*args, **kwarg):
-        return 0, lr_step
+        return lr_step
 
     return lr_func
 
@@ -72,9 +75,7 @@ def golden(x, eps, func):
     xx1 = x - l1 * grr
     xx2 = x - l2 * grr
     f1, f2 = func(xx1), func(xx2)
-    i = 4
     while (b - a) / 2 >= eps:
-        i += 1
         if f1 < f2:
             b, l2, f2 = l2, l1, f1
             l1 = a + k1 * (b - a)
@@ -85,7 +86,7 @@ def golden(x, eps, func):
             l2 = a + k2 * (b - a)
             xx2 = x - l2 * grr
             f2 = func(xx2)
-    return i, (a + b) / 2
+    return (a + b) / 2
 
 
 def using_grad_vector(method, x, eps, func):
@@ -96,12 +97,12 @@ def using_grad_vector(method, x, eps, func):
     i = 1
     while math.fabs(func_value - func_value_prev) > eps:
         func_value_prev = func_value
-        ii, lr = method(x, eps, func)
+        lr = method(x, eps, func)
         x = x - lr * gradient_in_the_point(func, x)
         steps_x.append(list(x))
         steps_y.append(func_value)
         func_value = func(x)
-        i += 1 + ii
+        i += 1
     return i, steps_x, steps_y
 
 
@@ -110,10 +111,10 @@ def method_mas(method, x, iteration_numb, func, eps=None):
         eps = 10
     xs = []
     ys = []
-    func_calls = 0
+    iterations = []
     for i in range(1, iteration_numb):
-        f_c, step_x, step_y = using_grad_vector(method, x, math.pow(eps, -i), func)
+        iter, step_x, step_y = using_grad_vector(method, x, math.pow(eps, -i), func)
         xs.append(step_x)
         ys.append(step_y)
-        func_calls += f_c
-    return func_calls, xs, ys
+        iterations.append(iter)
+    return iterations, xs, ys
