0b93b9255878c322364a0d2d53611955932887f8
natalia-nochevkina <90453557+natalia-nochevkina@users.noreply.github.com>
2023-03-22 22:55:19 +0300

    Update main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,11 +4,10 @@ import math
 
 def main():
 
-
     # analyze_random_function(800, 750)
-    # draw_random_function(5)
+    draw_random_function(4)
 
-    # multiple_tests_to_excel() # осторожно, работает очень-очень долго
+    #multiple_tests_to_excel() # осторожно, работает очень-очень долго
     # + необходимо заранее проверить, что существует папка ./tables
 
 
@@ -16,18 +15,18 @@ def main():
     # analyse_eps_parameter(used_func2, used_grad2)
     # analyse_eps_parameter(used_func3, used_grad3)
 
-    func_set = func_set_1
-    scaling_vector = [1 / 50, 1 / math.sqrt(13)]
-    method = 1
-
-    func1 = func_set[0]
-    grad1 = func_set[1]
-    func_name = func_set[-1]
-
-    func2 = scale_function(2, func1, scaling_vector)
-    grad2 = scale_function(2, grad1, scaling_vector)
-
-    functions_compare(func1, grad1, func2, grad2, method, func_name)
+    # func_set = func_set_3
+    # scaling_vector = [1 / math.sqrt(4.5), 1 / math.sqrt(2)]
+    # method = 1
+    #
+    # func1 = func_set[0]
+    # grad1 = func_set[1]
+    # func_name = func_set[-1]
+    #
+    # func2 = scale_function(2, func1, scaling_vector)
+    # grad2 = scale_function(2, grad1, scaling_vector)
+    #
+    # functions_compare(func1, grad1, func2, grad2, method, func_name)
 
 if __name__ == "__main__":
     main()
