af4e79769d8e21a46c454ae2c9f0bb387e8feef9
natalia-nochevkina <90453557+natalia-nochevkina@users.noreply.github.com>
2023-05-02 04:31:36 +0300

    Update errors_functions.py

--- Lab2/errors_functions.py
+++ Lab2/errors_functions.py
@@ -7,7 +7,6 @@ def error_func(ab, xy):
     err = np.asarray([error(ab, xy[i]) for i in range(len(xy))])
     return np.sum(err ** 2)
 
-
 def error_func_grad(ab, dot):
     a = error(ab, dot) * 2
     return [a * dot[0], a]
