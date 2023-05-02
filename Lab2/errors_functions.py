49219fa8ceedf8d420918ff3f7a07bd67a7d7d3f
natalia-nochevkina <nata.nochevkina@gmai.com>
2023-05-02 20:18:24 +0300

    names

--- Lab2/errors_functions.py
+++ Lab2/errors_functions.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def error(ab, dot):
-    return ab[0] * dot[0] + ab[1] - dot[1]
+def error(x, dot):
+    return x[0] * dot[0] + x[1] - dot[1]
 
-def error_func(ab, xy):
-    err = np.asarray([error(ab, xy[i]) for i in range(len(xy))])
+def error_func(x, data):
+    err = np.asarray([error(x, data[i]) for i in range(len(data))])
     return np.sum(err ** 2)
 
-def error_func_grad(ab, dot):
-    a = error(ab, dot) * 2
+def error_func_grad(x, dot):
+    a = error(x, dot) * 2
     return [a * dot[0], a]
