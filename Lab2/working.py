5ab5143284e957f78e95d9fd47bfa46b9ec52dff
natalia-nochevkina <nata.nochevkina@gmail.com>
2023-05-03 04:41:30 +0300

    some configs

--- Lab2/working.py
+++ Lab2/working.py
@@ -6,26 +6,28 @@ from errors_functions import *
 from regression_generation import *
 from regularization import *
 
+reg = NoRegularization()
 
-reg = Elastic()
+lr = const_learning_rate(0.1)
+gd       = GD      (lr=lr, regularization=reg)
 
 lr = exp_learning_rate(0.1)
-gd       = GD      (lr=lr, regularization=reg)
 momentum = Momentum(lr=lr, regularization=reg)
 nag      = NAG     (lr=lr, regularization=reg)
 
-lr = exp_learning_rate(50)
+# lr = exp_learning_rate(90)
+# lr = time_learning_rate(90)
+lr = step_learning_rate(90, 20)
 ada_grad = AdaGrad (lr=lr, regularization=reg)
 
-lr = exp_learning_rate(10)
+lr = const_learning_rate(10)
 rms_prop = RMSProp (lr=lr, regularization=reg)
 adam     = Adam    (lr=lr, regularization=reg)
 
-method = gd
 
 start = [100, 100]
 func = lambda x: 3 * x[0] ** 2 + x[1] ** 2
 grad = lambda x: [6 * x[0], 2 * x[1]]
 function = Function(func, grad)
 
-draw_levels(function, start, gd, momentum, nag, ada_grad)
+draw_levels(function, start, gd, momentum, nag, rms_prop)
