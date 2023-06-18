955955854f7f10ad7bc10ffc67ca0914d48dd877
natalia-nochevkina <nata.nochevkina@gmail.com>
2023-06-18 12:37:39 +0300

    abs

---
+++ Lab3/abs.py
@@ -0,0 +1,92 @@
+import logging
+import numpy as np
+from typing import Callable
+from numpy.linalg import pinv
+from abc import ABC, abstractmethod
+
+class absNewton(ABC):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self,
+                 function: Callable,
+                 max_iter: int = 1000,
+                 eps: float = 10 ** (-3),
+                 min_val: float = 10 ** (-9),
+                 init_data: np.ndarray = None):
+        self.function = function
+        self.max_iter = max_iter  # iteration limit to prevent infinity loop
+        self.eps = eps
+        self.min_val = min_val  # break-point, if reached, regression will be stopped
+        self.coefficients = None
+        self.x = None
+        self.y = None
+        self.init_data = None
+        if init_data is not None:
+            self.init_data = init_data
+
+    def recoverCoefs(self, x: np.ndarray, y: np.ndarray, init_data: np.ndarray = None):
+        self.x = x
+        self.y = y
+        if init_data is not None:
+            self.init_data = init_data
+
+        if init_data is None:
+            raise Exception("Data for computation expected")
+
+        self.coefficients = self.init_data
+        prev_square = 100
+        epoch = 1
+        iter = 0
+        while np.abs(prev_square - cur_square) >= self.eps and iter <= self.max_iter:
+            diverg = self.getDivergence()
+            sub_iter, sigma = self.getSigma(diverg)
+            iter = iter + sub_iter
+            self.coefficients = self.coefficients - sigma
+            cur_square = np.sqrt(np.sum(diverg ** 2))
+            self.logger.info(f"Epoch {epoch}: CUR {cur_square}")
+            delta = np.abs(prev_square - cur_square)
+            if delta < self.eps:
+                self.logger.info("Required error achieved")
+                return epoch, iter
+            if cur_square < self.min_val:
+                self.logger.info("Min val for square reached.")
+                return epoch, iter
+            prev_square = cur_square
+            epoch = epoch + 1
+        self.logger.info("Max number of iterations reached. Regression didn't converge.")
+
+        return epoch, iter
+
+    def getDivergence(self) -> np.ndarray:
+        return self.compute_divergence(self.coefficients)
+
+    def compute_divergence(self, coefficients: np.ndarray) -> np.ndarray:
+        y = self.function(self.x, coefficients)
+        return y - self.y
+
+    def get_computed_coefs(self) -> np.ndarray:
+        print(self.coefficients)
+        return self.function(self.x, self.coefficients)
+
+    def compute_jacobian(self, x0: np.ndarray, step: float = 10 ** (-6)):
+        y0 = self.compute_divergence(x0)
+        jacobian = []
+        sub_iter = 0
+        for i, parameter in enumerate(x0):
+            sub_iter = sub_iter + 1
+            x = x0.copy()
+            x[i] += step
+            y = self.compute_divergence(x)
+            derivative = (y - y0) / step
+            jacobian.append(derivative)
+        jacobian = np.array(jacobian).T
+        return sub_iter, jacobian
+
+    @staticmethod
+    def pseudo_inverse(x: np.ndarray) -> np.ndarray:
+        # Moore-Penrose inverse.
+        return pinv(x.T @ x) @ x.T
+
+    @abstractmethod
+    def getSigma(self, diverg):
+        pass
