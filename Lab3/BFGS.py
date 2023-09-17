a7e3c5a90d69347a3ad5f90066b7f44a3b7379c9
natalia-nochevkina <nata.nochevkina@gmail.com>
2023-09-17 23:46:25 +0300

    main upd

--- Lab3/BFGS.py
+++ Lab3/BFGS.py
@@ -1,45 +1,83 @@
 import math
 from abc import ABC, abstractmethod
+from typing import Callable
+
 import numpy as np
+
+from OptimizationMethods.Lab3.lib.min_methods import Method
 
 """"
 в точности функция wolfe из 1 лабы, изменено только название))
 """
 
+
 class absBFGS(ABC):
+    def __init__(self,
+                 function: Callable,
+                 eps: float = 10 ** (-3)):
+        self.coefficients = None
+        self.init_data = None
+        self.y = None
+        self.x = None
+        self.eps = eps
+        self.func = function
 
-    @abstractmethod
-    def execute(self, x, eps, f, grad):
-        pass
+    def recoverCoefs(self,
+                     x: np.ndarray,
+                     y: np.ndarray,
+                     init_data: np.ndarray = None):
+        self.x = x
+        self.y = y
+        self.init_data = init_data
+        data = np.dstack((x, y))[0]
+        i, result = self.execute(self.init_data, self.func)
+        self.coefficients = result[0]
+        return i, i
 
-    @staticmethod
-    def line_search(x, p, f, grad, c1=1e-4, c2=0.9, alf=1):
+    def grad(self, x):
+        delta = np.cbrt(np.finfo(float).eps)
+        dim = len(x)
+        nabl = np.zeros(dim)
+        for i in range(dim):
+            x_first = np.copy(x)
+            x_second = np.copy(x)
+            x_first[i] += delta
+            x_second[i] -= delta
+            nabl[i] = (self.func(x_first) - self.func(x_second)) / (2 * delta)
+        return nabl
+
+    def line_search(x, p, f, c1=1e-4, c2=0.9, alf=1):
         # Линейный поиск
-        nabl = grad(x)
+        nabl = absBFGS.grad(f, x)
         fx = f(x)
         new_x = x + alf * p
-        new_nabl = grad(new_x)
+        new_nabl = absBFGS.grad(f, new_x)
         f_new_x = f(new_x)
         while f_new_x > fx + (c1 * alf * nabl.T @ p) \
                 or math.fabs(new_nabl.T @ p) > c2 * math.fabs(nabl.T @ p) and f_new_x != fx:
             alf *= 0.5
             new_x = x + alf * p
             f_new_x = f(new_x)
-            new_nabl = grad(new_x)
+            new_nabl = absBFGS.grad(f, new_x)
         return alf
 
-class BFGS(absBFGS):
+    @abstractmethod
     def execute(self, x, eps, f, grad):
+        ...
+
+
+class BFGS(absBFGS):
+    def execute(self, x, eps, f):
         dim = len(x)
         steps_arg = [x]
         steps_f = [f(x)]
         i = 1
         aprox_hessian = np.eye(dim)
-        nabl = grad(x)
+        nabl = self.grad(x)
         while np.linalg.norm(nabl) > eps:
             p = -aprox_hessian @ nabl
-            alf = self.line_search(x, p, f, grad)
-            new_nabl = grad(x + alf * p)
+            alf = self.line_search(x, p, f)
+            new_nabl = self.grad(x + alf * p)
             y = new_nabl - nabl
             s = alf * p
             r = 1 / (y.T @ s)
@@ -55,17 +93,20 @@ class BFGS(absBFGS):
         return i, steps_arg, steps_f
 
 class L_BFGS(absBFGS):
-    def __init__(self, queue_size):
+    def __init__(self,
+                 function: Callable,
+                 queue_size: int = 50):
+        super().__init__(function)
         self.queue_sz = queue_size
 
-    def execute(self, x, eps, f, grad):
+    def execute(self, x, eps, f):
         steps_arg = [x]
         steps_f = [f(x)]
         queue_alpha = []
         queue_s_y_rho = []
         i = 1
         c1 = 10 ** (-3)
-        nabl = grad(x)
+        nabl = self.grad(x)
         grad_prev = nabl - nabl
 
         while np.linalg.norm(nabl) > eps:
--- Lab3/main.py
+++ Lab3/main.py
@@ -7,11 +7,12 @@ from OptimizationMethods.Lab3.BFGS import *
 from OptimizationMethods.Lab3.lib.batch_guys import *
 from OptimizationMethods.Lab3.lib.absNewton import *
 
-NOISE = 10
-init_coefs = [-33]
+NOISE = 1000
+DATA_SIZE = 100000
+init_coefs = [-33, 22, 0.1]
 
 def func(x, coeff):
-    return coeff[0] * cos(x)
+    return coeff[0] * cos(x) + coeff[1] * sin(x**2) + coeff[2] * x
 
 def funcToString(init_coefs):
     return "$ Initial: " + " + ".join([
@@ -20,10 +21,9 @@ def funcToString(init_coefs):
         for i in range(len(init_coefs))]) + "$"
 
 def main():
-    r = 99
-    x = np.arange(1, 1 + r)
+    x = np.arange(1, 1 + DATA_SIZE)
     y = func(x, init_coefs)
-    yn = y + NOISE * np.random.randn(r)
+    yn = y + NOISE * np.random.randn(DATA_SIZE)
     data = 10 * np.random.random(len(init_coefs))
 
     st = time.time_ns()
@@ -32,24 +32,28 @@ def main():
     method3 = NAG()
     method4 = Momentum()
     method5 = Golden()
-    mainMethod = method5 # основной метод
-    solver1 = DogLeg_Met(function=func, max_iter=10000, eps=10 ** (-5))
-    solver2 = GN_Met(function=func, max_iter=10000, eps=10 ** (-5))
-    solver3 = MiniBatch(function=func, max_iter=10000, eps=10 ** (-5), method=mainMethod)
-    # solver4 = BFGS()
-    mainSolver = solver2 # основной солвер
+    mainMethod = method4 # основной метод
+
+    solver1 = DogLeg_Met(function=func)
+    solver2 = GN_Met(function=func)
+    solver3 = Batch(function=func, method=mainMethod)
+    solver4 = MiniBatch(function=func, method=mainMethod)
+    solver5 = Stochastic(function=func, method=mainMethod)
+    solver6 = BFGS(function=func)
+    solver7 = L_BFGS(function=func)
+    mainSolver = solver7 # основной солвер
+
     epoch1, iters1 = solver1.recoverCoefs(x, yn, data)
     computed1 = solver1.get_computed_coefs()
     divergence1 = solver1.getDivergence()
     time1 = time.time_ns() - st
-
     string_func = funcToString(init_coefs)
 
     plt.figure()
     plt.plot(x, y, label="Initial function", linewidth=2)
     plt.plot(x, yn, label="Randomized data", linewidth=2)
     plt.plot(x, computed1, label="Computed: epoch " + str(epoch1) + "; real_iter " + str(iters1), linewidth=2)
-    # plt.plot(x, divergence1, label="Divergence", linewidth=2)
+    plt.plot(x, divergence1, label="Divergence", linewidth=2)
     plt.title(string_func)
     plt.xlabel("X")
     plt.ylabel("Y")
